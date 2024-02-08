7dc9515993a6a1ace20a3d6310b8f3c3274478b5
willslipknot <62850730+willslipknot@users.noreply.github.com>
2024-02-08 07:22:26 -0500

    segundo commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'http://localhost:3001',
-    //baseURL: 'https://raicesruralesback-production.up.railway.app',
+    //baseURL:'http://localhost:3001',
+    baseURL: 'https://raicesruralesback-production.up.railway.app',
+    
     withCredentials:true,
     
 })
