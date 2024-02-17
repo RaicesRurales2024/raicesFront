641b605b525a6f02bd162f48b9c1f62c69fbecee
RaicesRurales2024 <159819758+RaicesRurales2024@users.noreply.github.com>
2024-02-17 11:30:27 -0500

    Update axios.js ulti

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     //baseURL:'http://localhost:3001',
-    baseURL: 'https://raicesrurales.netlify.app',
+    baseURL: 'https://raicesrurales.onrender.com',
     
     withCredentials:true,
     
