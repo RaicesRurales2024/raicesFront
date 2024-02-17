dacd16143888b3649ef2161fd1f2a6260ffba178
RaicesRurales2024 <159819758+RaicesRurales2024@users.noreply.github.com>
2024-02-17 10:37:56 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     //baseURL:'http://localhost:3001',
-    baseURL: 'https://raicesruralesback-production.up.railway.app',
+    baseURL: 'https://app.netlify.com/teams/raicesrurales2024/builds/65d066e91a487475f881a77d',
     
     withCredentials:true,
     
