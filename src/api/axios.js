1c10d7457af344a071357c21ad31be81064b15ad
willslipknot <ruralesraices@gmail.com>
2024-02-17 13:08:55 -0500

    New

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    //baseURL:'http://localhost:3001',
-    baseURL: 'https://raicesrurales.onrender.com',
-    
+    baseURL:'http://localhost:3001',
+    //baseURL: 'https://raicesrurales.onrender.com',
     withCredentials:true,
     
 })
